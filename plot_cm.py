1aa27edaaf50a06afa20957f0bdd8d59ecc4b343
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2020-02-27 16:06:22 +0300

    Plot confusion matrix

---
+++ plot_cm.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+
+plt.style.use('ggplot')
+
+
+# Showing Confusion Matrix
+def plot_cm(y_true, y_pred, title, figsize=(5, 4)):
+    cm = confusion_matrix(y_true, y_pred, labels=np.unique(y_true))
+    cm_sum = np.sum(cm, axis=1, keepdims=True)
+    cm_perc = cm / cm_sum.astype(float) * 100
+    annot = np.empty_like(cm).astype(str)
+    nrows, ncols = cm.shape
+    for i in range(nrows):
+        for j in range(ncols):
+            c = cm[i, j]
+            p = cm_perc[i, j]
+            if i == j:
+                s = cm_sum[i]
+                annot[i, j] = '%.1f%%\n%d/%d' % (p, c, s)
+            elif c == 0:
+                annot[i, j] = ''
+            else:
+                annot[i, j] = '%.1f%%\n%d' % (p, c)
+    cm = pd.DataFrame(cm, index=np.unique(y_true), columns=np.unique(y_true))
+    cm.index.name = 'Actual'
+    cm.columns.name = 'Predicted'
+    fig, ax = plt.subplots(figsize=figsize)
+    plt.title(title)
+    sns.heatmap(cm, cmap="YlGnBu", annot=annot, fmt='', ax=ax)
